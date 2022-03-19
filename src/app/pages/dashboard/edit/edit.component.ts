b101cfdcf2c948a4167230ab451ee77a3bbfa077
Luis Neyra <luiscrazypeople@gmail.com>
2022-03-19 17:25:03 -0600

    Completed Edit Inventary

--- src/app/pages/dashboard/edit/edit.component.ts
+++ src/app/pages/dashboard/edit/edit.component.ts
@@ -20,6 +20,7 @@ export class EditComponent implements OnInit {
     code: new FormControl(undefined, [Validators.required]),
     description: new FormControl(undefined, [Validators.required]),
     stock: new FormControl(undefined, [Validators.required]),
+    id: new FormControl(undefined, [Validators.required]),
   });
   public isCreation$: Observable<boolean> = of(true);
   constructor(
@@ -30,77 +31,35 @@ export class EditComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-
     this.showProducts();
-   
   }
 
-  public showProducts(){
-    this.isCreation$ = this.activateRoute.params.pipe(
-      map((params) => (params['id'] ? false : true))
-    );
-    this.isCreation$.subscribe(console.log);
-    this.product$ = this.isCreation$.pipe(
-      switchMap((componentInvokationType) =>{
-        if(!componentInvokationType){
-          return this.activateRoute.params.pipe(
-              map((params) => params['id']),
-              switchMap((id: string) =>
-                this.firestore
-                  .doc<inventario>(`inventario/${id}`)
-                  .valueChanges({ idField: 'id' })
-              )
-            ) as Observable<inventario>;
-        }
-        return of(null);
-      })
+  public showProducts() {
+    this.product$ = this.activateRoute.params.pipe(
+      map((params) => params['id']),
+      switchMap((id: string) =>
+        this.firestore
+          .doc<inventario>(`inventario/${id}`)
+          .valueChanges({ idField: 'id' })
+      )
     ) as Observable<inventario>;
 
     this.product$.pipe(take(1)).subscribe((data) => {
-        if (data) {
-          this.productForm.patchValue(data);
-        }
-      });
-
-    // this.product$ = this.activateRoute.params.pipe(
-    //   map((params) => params['id']),
-    //   switchMap((id: string) =>
-    //     this.firestore
-    //       .doc<inventario>(`inventario/${id}`)
-    //       .valueChanges({ idField: 'id' })
-    //   )
-    // ) as Observable<inventario>;
-
-    // this.product$.pipe(take(1)).subscribe((data) => {
-    //   if (data) {
-    //     this.productForm.patchValue(data);
-    //   }
-    // });
+      if (data) {
+        this.productForm.patchValue(data);
+      }
+    });
   }
 
-  public async editProduct(product: FormGroup) {
+  public editProduct(product: FormGroup) {
     const products: inventario = product.value;
-    // this.isCreation$.pipe(take(1)).subscribe(async (isCreation) => {
-    //   if (isCreation) {
-    //     console.log('Es true ' + isCreation);
-        
-    //     products.id = this.firestore.createId();
-    //   }
-    //   try {
-    //     await this.serviceInventary.updateProduct(products);
-    //     alert('Producto actualizado');
-    //     this.route.navigateByUrl('admin');
-    //   } catch (err) {
-    //     window.alert(err);
-    //   }
-    // });
-
+    console.log(product);
     try {
-          await this.serviceInventary.updateProduct(products);
-          alert('Producto actualizado');
-          this.route.navigateByUrl('admin');
-        } catch (err) {
-          window.alert(err);
-        }
+      this.serviceInventary.updateProduct(products);
+      alert('Producto actualizado');
+      this.route.navigateByUrl('admin');
+    } catch (err) {
+      window.alert(err);
+    }
   }
 }
