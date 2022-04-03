a9bbbd65f310655487e6656911882c2eb29541ec
NeyraLucas <luiscrazypeople@gmail.com>
2022-04-03 13:58:22 -0500

    Modifique punto de venta

--- src/app/pages/dashboard/punto-de-venta/punto-de-venta.component.ts
+++ src/app/pages/dashboard/punto-de-venta/punto-de-venta.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Menu } from 'src/app/models/Menu.model';
 import { MenuService } from 'src/app/services/menu.service';
@@ -6,39 +8,41 @@ import { MenuService } from 'src/app/services/menu.service';
 @Component({
   selector: 'app-punto-de-venta',
   templateUrl: './punto-de-venta.component.html',
-  styleUrls: ['./punto-de-venta.component.scss']
+  styleUrls: ['./punto-de-venta.component.scss'],
 })
 export class PuntoDeVentaComponent implements OnInit {
-
   displayedColumns: string[] = ['imagen', 'nombre', 'desc', 'costo'];
-  dataSource: (Menu & { id: string; })[] = [];
+  dataSource: (Menu & { id: string })[] = [];
   clickedRows = new Set<Menu>();
   total: number = 0;
-
+  subTotal: number = 0;
+  dataMenu: Menu[] = [];
   selectedValue!: string;
   dataMenu$!: Observable<Array<Menu>>;
 
   foods: any = [
-    {value: 'steak-0', viewValue: 'Efectivo'},
-    {value: 'pizza-1', viewValue: 'Tarjeta'},
-    {value: 'tacos-2', viewValue: 'Paypal'},
+    { value: 'steak-0', viewValue: 'Efectivo' },
+    { value: 'pizza-1', viewValue: 'Tarjeta' },
+    { value: 'tacos-2', viewValue: 'Paypal' },
   ];
 
-
-  constructor(private readonly sMenu:MenuService) { }
+  constructor(private readonly sMenu: MenuService, private route: Router) {}
 
   ngOnInit(): void {
-     this.sMenu.showMenuAll().subscribe(
-       (data) => {
-        this.dataSource =  data;
-        console.log(this.dataSource);
-
-       }
-     )
+    this.sMenu.showMenuAll().subscribe((data) => {
+      this.dataSource = data;
+    });
   }
 
-  public test(){
+  public test(data: Menu) {
+    this.dataMenu.push(data);
+    this.total++;
+    this.subTotal += data.price;
+  }
 
+  public payMenu(){
+    window.alert('Producto pagado');
+    this.route.navigateByUrl('admin');
   }
 
 }
