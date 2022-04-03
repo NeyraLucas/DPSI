775bc38166ac088fff39c9ee4a445fdbdf630b3c
NeyraLucas <luiscrazypeople@gmail.com>
2022-04-03 16:15:46 -0500

    Created order, just one

---
+++ src/app/pages/dashboard/ordenes/ordenes.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+
+
+@Component({
+  selector: 'app-ordenes',
+  templateUrl: './ordenes.component.html',
+  styleUrls: ['./ordenes.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
+})
+
+
+export class OrdenesComponent implements OnInit {
+
+  dataSource = ELEMENT_DATA;
+  columnsToDisplay = ['name', 'weight', 'symbol', 'position'];
+  expandedElement!: PeriodicElement | null;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+  description: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {
+    position: 1,
+    name: 'Hydrogen',
+    weight: 1.0079,
+    symbol: 'H',
+    description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
+        atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`,
+  },
+  {
+    position: 2,
+    name: 'Helium',
+    weight: 4.0026,
+    symbol: 'He',
+    description: `Helium is a chemical element with symbol He and atomic number 2. It is a
+        colorless, odorless, tasteless, non-toxic, inert, monatomic gas, the first in the noble gas
+        group in the periodic table. Its boiling point is the lowest among all the elements.`,
+  },
+];
--- src/app/pages/dashboard/punto-de-venta/punto-de-venta.component.ts
+++ src/app/pages/dashboard/punto-de-venta/punto-de-venta.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Menu } from 'src/app/models/Menu.model';
 import { MenuService } from 'src/app/services/menu.service';
+import { OrdenesService } from 'src/app/services/ordenes.service';
 
 @Component({
   selector: 'app-punto-de-venta',
@@ -16,17 +17,24 @@ export class PuntoDeVentaComponent implements OnInit {
   clickedRows = new Set<Menu>();
   total: number = 0;
   subTotal: number = 0;
-  dataMenu: Menu[] = [];
+  dataMenu!: Menu;
   selectedValue!: string;
   dataMenu$!: Observable<Array<Menu>>;
 
   foods: any = [
-    { value: 'steak-0', viewValue: 'Efectivo' },
-    { value: 'pizza-1', viewValue: 'Tarjeta' },
-    { value: 'tacos-2', viewValue: 'Paypal' },
+    { value: '1', viewValue: '1' },
+    { value: '2', viewValue: '2' },
+    { value: '3', viewValue: '3' },
+    { value: '4', viewValue: '4' },
+    { value: '5', viewValue: '5' },
+    { value: '6', viewValue: '6' },
   ];
 
-  constructor(private readonly sMenu: MenuService, private route: Router) {}
+  constructor(
+    private readonly sMenu: MenuService,
+    private route: Router,
+    private ordersService: OrdenesService
+  ) {}
 
   ngOnInit(): void {
     this.sMenu.showMenuAll().subscribe((data) => {
@@ -35,14 +43,29 @@ export class PuntoDeVentaComponent implements OnInit {
   }
 
   public test(data: Menu) {
-    this.dataMenu.push(data);
-    this.total++;
-    this.subTotal += data.price;
+      //console.log(data);
+      // this.dataMenu.id = data.id;
+      // this.dataMenu.description = data.description;
+      // this.dataMenu.image = data.image;
+      // this.dataMenu.name = data.image;
+      // this.dataMenu.price = data.price;
+      // this.total++;
+      // this.subTotal += data.price;
+      this.dataMenu = data;
+      console.log(`Total: ${this.total}`);
+      console.log(this.dataMenu);
+
+
   }
 
-  public payMenu(){
-    window.alert('Producto pagado');
-    this.route.navigateByUrl('admin');
+  public payMenu() {
+    // let json:any = JSON.stringify(this.dataMenu);
+    try {
+      this.ordersService.CreateOrder(this.dataMenu);
+      window.alert('Producto pagado');
+      this.route.navigateByUrl('admin');
+    } catch (err) {
+      window.alert(err);
+    }
   }
-
 }
